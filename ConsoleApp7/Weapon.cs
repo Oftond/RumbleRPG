befd3824d6ad5c1aae363868da00cfe3889a2c61
K-301-5\STUDENT <Kirmeleh@yndex.ru>
2024-12-04 15:05:06 +0700

    Monsters add

---
+++ ConsoleApp7/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Weapon
+{
+    public string Name { get; }
+    public int minDamage { get; private set; }
+    public int maxDamage { get; private set; }
+
+    public Weapon(string name)
+    {
+        Name = name;
+    }
+
+    public Weapon(string name, int n1, int n2) : this(name)
+    {
+        SetDamageParams(n1, n2);
+    }
+
+    public void SetDamageParams(int number1, int number2)
+    {
+        if (number1 > number2)
+        {
+            (minDamage, maxDamage) = (number2, number1);
+            Console.WriteLine("Неправельный ввод " + Name);
+        }
+        else
+        {
+            (minDamage, maxDamage) = (number1, number2);
+        }
+
+        if (minDamage < 1)
+        {
+            minDamage = 1;
+        }
+
+        if (maxDamage <= 1)
+        {
+            maxDamage = 10;
+        }
+    }
+
+    public int GetDamage() => (minDamage + maxDamage) / 2;
+}
+
---
+++ ConsoleApp7/enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class enemy : ICharacter
+{
+    public event Action<enemy> onDath;
+    public event Action<enemy> damage;
+    Random random = new Random();
+    private int _baseDamage = 5;
+    private int _hp;
+    public Weapon weapon;
+    public int HP { get => _hp; private set => _hp = value; }
+    public int Damage {
+        get
+        {
+            if (weapon == null)
+            {
+                return _baseDamage;
+            }
+            else
+            {
+                return weapon.GetDamage() + _baseDamage;
+            }
+        }
+        set => _baseDamage = value;
+    }
+    public int Armores { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Name { get; }
+    
+}
